c70a1a992e2a607d1c86bfe6210c27690d94bc48
leehoon <leehoon@kaist.ac.kr>
2019-10-20 01:01:32 +0900

    asdf

--- src/userprog/process.c
+++ src/userprog/process.c
@@ -49,7 +49,7 @@ process_execute (const char *file_name)
   tid = thread_create (command, PRI_DEFAULT, start_process, fn_copy);
   if (tid == TID_ERROR)
     palloc_free_page (fn_copy);
-  return process_wait(tid);
+  return tid;
 }
 
 /* A thread function that loads a user process and starts it
