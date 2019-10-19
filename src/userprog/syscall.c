a32dc73763d5e8239336e96bdad314903945e6a3
leehoon <leehoon@kaist.ac.kr>
2019-10-19 21:05:02 +0900

    adsf

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -112,7 +112,10 @@ int my_wait(pid_t pid){
 
 bool my_create(const char *file, unsigned initial_size){
   if(file == NULL) my_exit(-1);
-  check_address(file);
+  //check_address(file);
+  if(!is_kernel_vaddr(addr)){
+    my_exit(-1);
+  }
   return filesys_create(file, initial_size);
 }
 
