1d2fa0c59fcc18287a734ebfa2271d8d82dac242
leehoon <leehoon@kaist.ac.kr>
2019-10-19 16:56:30 +0900

    a

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -9,48 +9,50 @@ static void syscall_handler (struct intr_frame *);
 int write(int fd, const void *buffer, unsigned size);
 
 void
-syscall_init (void) 
+syscall_init (void)
 {
   intr_register_int (0x30, 3, INTR_ON, syscall_handler, "syscall");
 }
 
 static void
-syscall_handler (struct intr_frame *f UNUSED) 
+syscall_handler (struct intr_frame *f UNUSED)
 {
   uint32_t *esp = f->esp;
   printf ("system call! %d \n", *esp);
   hex_dump(esp, esp, 300, true);
-  
-  if(*esp == SYS_HALT){
 
+  if(*esp == SYS_HALT){ // 0
+    halt();
   }
-  else if(*esp == SYS_EXIT){
-    //hex_dump(esp, esp, 300, true);
+  else if(*esp == SYS_EXIT){ // 1
     printf("%s: exit(0)\n", thread_name());
-    //printf("thread exit...\n");
     thread_exit();
-  }else if(*esp == SYS_EXEC){
+  }else if(*esp == SYS_EXEC){ // 2
 
-  }else if(*esp == SYS_WAIT){
+  }else if(*esp == SYS_WAIT){ // 3
 
-  }else if(*esp == SYS_WRITE){
-//    printf("esp ::: %p \n", esp);
-//    printf("%d \n", (f->eax));
-//    hex_dump(esp, esp, 300, true);
+  }else if(*esp == SYS_CREATE){ // 4
+
+  }else if(*esp == SYS_REMOVE){ // 5
+
+  }else if(*esp == SYS_OPEN){ // 6
+
+  }else if(*esp == SYS_FILESIZE){ // 7
+
+  }else if(*esp == SYS_READ){ // 8
+
+  }else if(*esp == SYS_WRITE){ // 9
     int fd = (int)*(uint32_t *)(f->esp+4);
     const void *buf = (void *)*(uint32_t *)(f->esp+8);
     unsigned size = (unsigned)*(uint32_t *)(f->esp+12);
     f->eax = write(fd, buf, size);
-    //write(fd, buf, size);
-//    printf("%d  ", *(esp+1));
-//    printf("%s  ", *(esp+2));
-//    printf("%u  ", *(esp+3));
-   // printf("\n\n, %", (int)*(uint32_t *)(esp+4));
-   // printf("\n\n, %p", (void *)*(uint32_t *)(esp+8));
-   // printf("\n\n, %p", *(esp+12));
-  //  printf("good..");
+  }else if(*esp == SYS_SEEK){ // 10
+
+  }else if(*esp == SYS_TELL){ // 11
+
+  }else if(*esp == SYS_CLOSE){ // 12
+
   }
-  //thread_exit ();
 }
 
 int write(int fd, const void *buffer, unsigned size){
