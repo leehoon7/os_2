04cd473f213606ce31130287bd208f5842b8b0bc
woojune511 <geonju511@naver.com>
2019-11-11 01:12:39 +0900

    요구 페이징 할일 1

--- src/userprog/exception.c
+++ src/userprog/exception.c
@@ -154,17 +154,35 @@ page_fault (struct intr_frame *f)
   write = (f->error_code & PF_W) != 0;
   user = (f->error_code & PF_U) != 0;
 
-  if(!user || is_kernel_vaddr(fault_addr) || not_present){
+  check_address(fault_addr, f->esp);
+  if(!not_present)
     my_exit(-1);
+  struct vm_entry* vme = find_vme(fault_addr);
+  if(vme){
+    load = handle_mm_fault(vme); //handle_mm_fault 구현 해야 됨
+    if(!vme->is_loaded)
+      printf("fault_addr is not loaded on physical memory(page fault)\n");
   }
+
+  if(!load){
+    printf ("Page fault at %p: %s error %s page in %s context.\n",
+            fault_addr,
+            not_present ? "not present" : "rights violation",
+            write ? "writing" : "reading",
+            user ? "user" : "kernel");
+    kill(f); //my_exit(-1)?
+  }
+  // if(!user || is_kernel_vaddr(fault_addr) || not_present){
+  //   my_exit(-1);
+  // }
   /* To implement virtual memory, delete the rest of the function
      body, and replace it with code that brings in the page to
      which fault_addr refers. */
-  printf ("Page fault at %p: %s error %s page in %s context.\n",
-          fault_addr,
-          not_present ? "not present" : "rights violation",
-          write ? "writing" : "reading",
-          user ? "user" : "kernel");
-  my_exit(-1);
+  // printf ("Page fault at %p: %s error %s page in %s context.\n",
+  //         fault_addr,
+  //         not_present ? "not present" : "rights violation",
+  //         write ? "writing" : "reading",
+  //         user ? "user" : "kernel");
+  // my_exit(-1);
   //kill (f);
 }
