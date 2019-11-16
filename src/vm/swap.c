2b022276c41e41e3b35d39db8b9afbf60c43dcd4
woojune511 <geonju511@naver.com>
2019-11-16 15:56:54 +0900

    asfd

--- src/vm/swap.c
+++ src/vm/swap.c
@@ -1,3 +1,13 @@
-#include "vm/page.h"
-#include <list.h>
-#include "threads/synch.h"
+#include "vm/swap.h"
+
+void swap_init(size_t used_index, void* kaddr){
+
+}
+
+void swap_in(size_t used_index, void* kaddr){
+
+}
+
+size_t swap_out(void* kaddr){
+
+}
--- src/vm/swap.h
+++ src/vm/swap.h
@@ -1,1 +1,7 @@
-// include???
+#include "vm/page.h"
+#include <list.h>
+#include "threads/synch.h"
+
+void swap_init(size_t used_index, void* kaddr);
+void swap_in(size_t used_index, void* kaddr);
+size_t swap_out(void* kaddr);
