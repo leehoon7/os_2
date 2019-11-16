f9b0f513efc42c496510164660d3fa616cc22e6b
leehoon <leehoon@kaist.ac.kr>
2019-11-16 16:35:30 +0900

    swap_in

--- src/vm/swap.c
+++ src/vm/swap.c
@@ -8,10 +8,23 @@
 struct bitmap *swap_bitmap;
 struct block *swap_block;
 
-void swap_init(void)
-{
+void swap_init(void){
 	swap_block = block_get_role(BLOCK_SWAP);
   size_t size = block_size(swap_block) / SECTORS_PER_PAGE;
 	swap_bitmap = bitmap_create(size);
 	bitmap_set_all(swap_bitmap, true);
+}
+
+void swap_in(size_t used_index, void* kaddr){
+  // 현재 쓰이고 있으면 true, 안쓰이고 있으면 false
+  bool using_now = bitmap_test(swap_map, used_index);
+  if(using_now){
+    return;
+  }
+
+  int i;
+  for(i = 0; i < SECTORS_PER_PAGE; i++){
+    block_read(swap_block, used_index * SECTORS_PER_PAGE + i, page + (BLOCK_SECTOR_SIZE * i));
+  }
+  bitmap_set(swap_bitmap, used_index, true);
 }
