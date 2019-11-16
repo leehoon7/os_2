a2f73721231dec89c66dab4abb2a90480ecf2704
leehoon <leehoon@kaist.ac.kr>
2019-11-16 15:56:33 +0900

    swap_init

--- src/vm/swap.c
+++ src/vm/swap.c
@@ -1,3 +1,17 @@
+#include "vm/swap.h"
 #include "vm/page.h"
 #include <list.h>
 #include "threads/synch.h"
+#include "devices/block.h"
+#include "lib/kernel/bitmap.h"
+
+struct bitmap *swap_bitmap;
+struct block *swap_block;
+
+void swap_init(void)
+{
+	swap_block = block_get_role(BLOCK_SWAP);
+  size_t size = block_size(swap_block) / SECTORS_PER_PAGE;
+	swap_bitmap = bitmap_create(size);
+	bitmap_set_all(swap_bitmap, true);
+}
--- src/vm/swap.h
+++ src/vm/swap.h
@@ -1,1 +1,6 @@
 // include???
+#define SECTORS_PER_PAGE 8
+
+void swap_init(size_t used_index, void* kaddr);
+void swap_in(size_t used_index, void* kaddr);
+size_t swap_out(void* kaddr);
